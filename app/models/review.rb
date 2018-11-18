a154e34c674718b3a344752e2eb40b2168f436cb
Michael Roose <mjroose@gmail.com>
2018-11-18 18:53:58 +0000

    automatically backed up by learn

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,7 +8,7 @@ class Review < ActiveRecord::Base
   validate :trip_completed
 
   def trip_completed
-    if self.reservation == nil || self.reservation.status != "accepted" || self.reservation.checkout < Date.today
+    if self.reservation == nil || self.reservation.status != "accepted" || self.reservation.checkout > Date.today
       binding.pry
       errors[:reservation] = "must have actually stayed at listing"
     end
